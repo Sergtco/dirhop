fd0842c3b88e040ccae8ed5581164a713d1e2be6
Ivan Nizelnik <iva.nizelnik@gmail.com>
2025-04-14 23:06:36 +0300

    chore(tui): extract tui to module - add terminal detection

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,18 @@ use std::{
     collections::BTreeMap,
     env,
     fs::{self, DirEntry},
-    io::{self, Write},
+    io::{self},
+    process::exit,
 };
 
 use crossterm::{
-    ExecutableCommand, cursor,
+    cursor,
     event::{Event, KeyModifiers},
-    execute,
-    style::Stylize,
-    terminal,
+    tty::IsTty,
 };
+
+mod tui;
+use tui::{Rect, Renderer};
 
 struct Labeler(u16);
 
@@ -37,41 +39,9 @@ impl Iterator for Labeler {
         Some(String::from_utf8(vec![first_letter, second_letter]).expect("valid utf8 chars"))
     }
 }
+type Binds = BTreeMap<String, DirEntry>;
 
-fn restore() -> io::Result<()> {
-    clear()?;
-
-    terminal::disable_raw_mode()
-}
-
-fn clear() -> io::Result<()> {
-    execute!(
-        io::stderr(),
-        cursor::RestorePosition,
-        terminal::Clear(terminal::ClearType::FromCursorDown)
-    )?;
-
-    Ok(())
-}
-
-fn draw_list(ans: &str, binds: &BTreeMap<String, DirEntry>) -> io::Result<()> {
-    let mut stderr = io::stderr();
-    stderr.execute(cursor::SavePosition)?;
-
-    for (label, entry) in binds.iter() {
-        let label = label.strip_prefix(ans).unwrap_or(&label);
-        write!(
-            stderr,
-            "[{}{}]{}\r\n",
-            ans.blue(),
-            label,
-            entry.path().to_string_lossy()
-        )?;
-    }
-    Ok(())
-}
-
-fn match_binds(ans: &str, binds: BTreeMap<String, DirEntry>) -> BTreeMap<String, DirEntry> {
+fn match_binds(ans: &str, binds: Binds) -> Binds {
     binds
         .into_iter()
         .filter(|(label, _)| label.starts_with(ans))
@@ -79,36 +49,46 @@ fn match_binds(ans: &str, binds: BTreeMap<String, DirEntry>) -> BTreeMap<String,
 }
 
 fn main() -> io::Result<()> {
+    if !io::stderr().is_tty() {
+        eprintln!("This is not a terminal, Babe!");
+        exit(1);
+    }
+
     let mut args = env::args();
     args.next();
 
-    let path = match args.next() {
-        Some(val) => val,
-        None => ".".to_string(),
-    };
+    let path = args.next().unwrap_or(".".to_string());
 
     let entries = fs::read_dir(&path)?.filter_map(|entry| entry.ok());
     let labels = Labeler::new().into_iter();
 
-    let mut binds = labels.zip(entries.into_iter()).collect::<BTreeMap<_, _>>();
+    let mut binds = labels.zip(entries.into_iter()).collect::<Binds>();
 
-    terminal::enable_raw_mode()?;
+    let mut renderer = {
+        let (x, y) = cursor::position()?;
+        Renderer::with_bounds(Rect {
+            x,
+            y,
+            width: 10,
+            height: 10,
+        })?
+    };
 
     let mut ans = String::new();
-    draw_list(&ans, &binds)?;
+    renderer.draw_list(&ans, &binds)?;
 
     while ans.len() < 2 {
         match crossterm::event::read()? {
             Event::Key(event) => {
                 if event.code.is_char('c') && event.modifiers == KeyModifiers::CONTROL {
-                    restore()?;
+                    renderer.restore()?;
                 }
                 if let Some(key) = event.code.as_char() {
                     if event.modifiers.is_empty() {
                         ans.push(key);
                         binds = match_binds(&ans, binds);
-                        clear()?;
-                        draw_list(&ans, &binds)?;
+
+                        renderer.draw_list(&ans, &binds)?;
                     }
                 }
             }
@@ -116,8 +96,7 @@ fn main() -> io::Result<()> {
         }
     }
 
-    clear()?;
-    terminal::disable_raw_mode()?;
+    renderer.restore()?;
 
     if let Some(entry) = binds.get(&ans) {
         println!("{}", entry.path().to_string_lossy());
---
+++ src/tui.rs
@@ -0,0 +1,87 @@
+use std::{
+    collections::BTreeMap,
+    fs::DirEntry,
+    io::{self, Write},
+};
+
+use crossterm::{
+    ExecutableCommand, cursor, execute, queue,
+    style::{self, Stylize},
+    terminal::{self, ClearType},
+};
+
+#[derive(Debug, Clone, Copy)]
+pub struct Rect {
+    pub x: u16,
+    pub y: u16,
+    pub width: u16,
+    pub height: u16,
+}
+
+#[derive(Debug)]
+pub struct Renderer {
+    stderr: io::Stderr,
+    bounds: Rect,
+}
+
+impl Renderer {
+    pub fn with_bounds(bounds: Rect) -> io::Result<Self> {
+        terminal::enable_raw_mode()?;
+
+        Ok(Self {
+            stderr: io::stderr(),
+            bounds,
+        })
+    }
+
+    pub fn draw_list(&mut self, ans: &str, binds: &BTreeMap<String, DirEntry>) -> io::Result<()> {
+        self.clear_rect(self.bounds)?;
+        self.stderr
+            .execute(cursor::MoveTo(self.bounds.x, self.bounds.y))?;
+
+        for (label, entry) in binds.iter().take(self.bounds.height as usize) {
+            let label = label.strip_prefix(ans).unwrap_or(&label);
+            queue!(
+                self.stderr,
+                style::Print(format!(
+                    "[{}{}]{}\r\n",
+                    ans.blue(),
+                    label,
+                    entry.path().to_string_lossy()
+                )),
+            )?;
+        }
+
+        self.stderr.flush()?;
+        Ok(())
+    }
+
+    pub fn restore(&mut self) -> io::Result<()> {
+        self.clear_all()?;
+        terminal::disable_raw_mode()
+    }
+
+    fn clear_rect(&mut self, area: Rect) -> io::Result<()> {
+        let spaces = " ".repeat(area.width as usize);
+        for y in self.bounds.y..(self.bounds.y + self.bounds.height) {
+            queue!(
+                self.stderr,
+                cursor::MoveTo(self.bounds.x, y),
+                style::Print(spaces.clone()),
+            )?;
+        }
+
+        self.stderr.flush()?;
+        Ok(())
+    }
+
+    pub fn clear_all(&mut self) -> io::Result<()> {
+        execute!(
+            self.stderr,
+            cursor::MoveTo(self.bounds.x, self.bounds.y),
+            terminal::Clear(ClearType::FromCursorDown)
+        )?;
+
+        Ok(())
+    }
+}
